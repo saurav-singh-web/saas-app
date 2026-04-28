8a8fd1eaa292dc4af02ddaac2882b90ca1169b86
Saurav Singh <sauravsingh98866@gmail.com>
2026-04-29 01:27:07 +0530

    add protected routes, clean up route structure

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from './ProtectedRoute'
 import Login from '../pages/Login'
 import Dashboard from '../pages/Dashboard'
 import PostsFeed from '../pages/PostsFeed'
@@ -7,26 +8,42 @@ import OrgPage from '../pages/OrgPage'
 function AppRoutes({ user, showToast }) {
   return (
     <Routes>
+      {/* Public routes */}
+      <Route
+        path="/login"
+        element={
+          user
+            ? <Navigate to="/" replace />
+            : <Login showToast={showToast} />
+        }
+      />
+
       <Route
         path="/posts"
         element={<PostsFeed user={user} showToast={showToast} />}
       />
+
+      {/* Protected routes */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute user={user}>
+            <Dashboard user={user} showToast={showToast} />
+          </ProtectedRoute>
+        }
+      />
+
       <Route
         path="/org/:slug"
         element={
-          user
-            ? <OrgPage user={user} showToast={showToast} />
-            : <Login showToast={showToast} />
+          <ProtectedRoute user={user}>
+            <OrgPage user={user} showToast={showToast} />
+          </ProtectedRoute>
         }
       />
-      <Route
-        path="*"
-        element={
-          user
-            ? <Dashboard user={user} showToast={showToast} />
-            : <Login showToast={showToast} />
-        }
-      />
+
+      {/* Catch all — redirect to home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
